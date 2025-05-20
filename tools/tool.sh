e94add20634aff69c10c5e2a69b5bd9e614240cf
chaithanya <chaithanya1812@gmail.com>
2025-05-20 11:53:49 +0530

    montioring

--- tools/tool.sh
+++ tools/tool.sh
@@ -1,1 +1,1 @@
-ansible -i ${1}-internal.chaithanya.cloud,  -e ansible_user=centos -e ansible_password=DevOps321 -e tool_name=${1} setup.yaml
+ansible -i ${1}-internal.chaithanya.cloud  -e ansible_user=centos -e ansible_password=DevOps321 -e tool_name=${1} setup.yaml
