94a55457d26cc6aac8bfa7fb8c00ef7c6c60ed5d
chaithanya <chaithanya1812@gmail.com>
2025-05-20 20:10:33 +0530

    montioring

--- terraform/modules/sns/main.tf
+++ terraform/modules/sns/main.tf
@@ -1,3 +1,10 @@
 resource "aws_sns_topic" "user_updates" {
   name = "observability-topic"
 }
+
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.user_updates.arn
+  protocol  = "email"
+  endpoint  = "chaithanyadasari63@gmail.com"
+}
