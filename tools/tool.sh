9714e28c26d8f57ba22eca17738eb2432dda1dbf
chaithanya <chaithanya1812@gmail.com>
2025-06-12 14:38:23 +0530

    montioring

--- tools/tool.sh
+++ tools/tool.sh
@@ -1,4 +1,4 @@
 
 git pull
 
-ansible-playbook -i ${1}-internal.chaithanya.cloud,  -e ansible_user=centos -e ansible_password=DevOps321 -e tool_name=${1} setup.yaml
+ansible-playbook -i 13.221.117.252,  -e ansible_user=centos -e ansible_password=DevOps321 -e tool_name=${1} setup.yaml
